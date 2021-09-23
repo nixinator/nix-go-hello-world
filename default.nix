e11e2df542362ffeafd24b0ff759037659a9b892
nixinator <33lockdown33@protonmail.com>
2021-09-23 20:16:33 +0100

    use a more recent nixpkgs ho ho

--- default.nix
+++ default.nix
@@ -1,10 +1,7 @@
 with  import (builtins.fetchTarball {
-  # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2018-09-12";
-  # Commit hash for nixos-unstable as of 2018-09-12
-  url = "https://github.com/nixos/nixpkgs/archive/ca2ba44cab47767c8127d1c8633e2b581644eb8f.tar.gz";
+  url = "https://github.com/nixos/nixpkgs/archive/7e9b0df.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1jg7g6cfpw8qvma0y19kwyp549k1qyf11a5sg6hvn6awvmkny47v";
+  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
 }) {};
 {
 	devEnv = stdenv.mkDerivation {
