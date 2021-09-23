361a7ee93fc7000f2e5a81ff76636eb3693e473d
nixinator <33lockdown33@protonmail.com>
2021-09-23 20:03:22 +0100

    pin a old nixpkgs version for herc

--- default.nix
+++ default.nix
@@ -1,4 +1,12 @@
-with import <nixpkgs> {}; {
+with  import (builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-2018-09-12";
+  # Commit hash for nixos-unstable as of 2018-09-12
+  url = "https://github.com/nixos/nixpkgs/archive/ca2ba44cab47767c8127d1c8633e2b581644eb8f.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1jg7g6cfpw8qvma0y19kwyp549k1qyf11a5sg6hvn6awvmkny47v";
+}) {};
+{
 	devEnv = stdenv.mkDerivation {
 		name = "hello-world";
 		src = ./.;
